5537f2ec35f40296f5e3e08f7203d6ab7660334f
Xuan_Truong <bichkhoai2001@gmail.com>
2022-08-26 15:27:16 +0700

    lan 1

---
+++ app/src/main/java/com/example/b9_room/data/UserRepository.kt
@@ -0,0 +1,25 @@
+package com.example.b9_room.data
+
+import androidx.lifecycle.LiveData
+
+class UserRepository(private val userDao: UserDao) {
+
+    val readAllData: LiveData<List<User>> = userDao.readAllData()
+
+    suspend fun addUser(user: User){
+        userDao.addUser(user)
+    }
+
+    suspend fun updateUser(user: User){
+        userDao.updateUser(user)
+    }
+
+    suspend fun deleteUser(user: User){
+        userDao.deleteUser(user)
+    }
+
+    suspend fun deleteAllUsers(){
+        userDao.deleteAllUsers()
+    }
+
+}
